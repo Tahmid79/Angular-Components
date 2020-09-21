70f9149fb5eac35a70dfac2ca95169b506b75563
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-09-21 19:39:08 +0600

    Stop Event Bubbling

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
 
+ modalOpen = false ;
+
+  onClick(){
+    this.modalOpen = ! this.modalOpen ;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
